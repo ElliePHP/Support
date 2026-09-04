e1662812fff8df53f12c2c665c59ca2086b3e0df
Joey 😎 <43940776+joeyboli@users.noreply.github.com>
2026-09-04 20:55:40 +0000

    Delete index.php

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use ElliePHP\Components\Support\Constants\HashAlgos;
-use ElliePHP\Components\Support\Util\Hash;
-
-require_once 'vendor/autoload.php';
-
-
-echo Hash::hash('33333', HashAlgos::XXH3);
